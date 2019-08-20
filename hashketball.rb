f6b3c31bd91fa0a108d52a9ec42d0b3ba181f1d6
Nicholas Feitel <nicholas.feitel@gmail.com>
2019-08-20 14:06:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -253,6 +253,13 @@ long_guy
 end
 
 def long_name_steals_a_ton?
+  long_guy = nil
+  name_length = 0
+  steals= 0
+  game_hash.each do |team, team_info|
+    team_info[:players].each do |player|
+      if player.keys.join.length > name_length
+        name_length = player.keys.join.length 
   
 end
 
